4c4e7423a63e3582b443c2e16e815f029b4d4e66
SiddhantJha317 <email@example.com>
2024-07-07 22:17:13 +0530

    completed lasso regression and working on elastic net

---
+++ scratchml/linear_model/elasticnet_regression.py
@@ -0,0 +1,7 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation, PillowWriter
+
+class ElasticNet:
+    def __init__(self,train,lr,l1,l2):
+        
--- scratchml/linear_model/lasso_regression.py
+++ scratchml/linear_model/lasso_regression.py
@@ -2,36 +2,36 @@ import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation, PillowWriter
 
+class LassoRegression:
 
-class RidgeRegression:
-    def __init__(self, lr, iter, l2):
+    def __init__(self,iter,lr,l1):
         self.lr = lr
         self.iter = iter
-        self.l2 = l2
+        self.l1 = l1
         self.m = None
         self.b = None
         self.loss = []
         self.history =[]
     
-    def prediction(self, X):
-        pred = np.dot(X, self.m) + self.b
+    def prediction(self,X):
+        pred = np.dot(X,self.m)+self.b
         return pred
     
-    def cost_func(self, Y, pred):
-        diff = Y - pred
-        cost = np.mean(diff**2) + self.l2 * np.mean(self.m**2)
+    def cost_func(self,Y, pred):
+        diff = Y-pred
+        cost = np.mean(diff**2)+self.l1*np.sum(np.abs(self.m))
         return cost
     
-    def back_prop(self, Y, pred, X):
-        diff = Y - pred
-        dw = -2 * np.dot(X.T, diff) / X.shape[0] + 2 * self.l2 * self.m
+    def back_prop(self,Y,pred,X):
+        diff = Y-pred
+        dw = -2 * np.dot(X.T,diff)/X.shape[0] + self.l1*np.sign(self.m)
         db = -2 * np.mean(diff)
         return dw, db
-    
-    def train(self, X, Y):
+
+    def train(self,X,Y):
         n_samples, n_features = X.shape
         self.b = 0
-        self.m = np.random.randn(n_features, 1)  
+        self.m = np.random.rand(n_features, 1)  
         
         for i in range(self.iter):
             y_pred = self.prediction(X)
@@ -44,8 +44,7 @@ class RidgeRegression:
             print(f"Iteration: {i + 1}, Loss: {cost}")
             self.history.append((self.m.copy(),self.b))
         return self
-    
-    def animate(self, X, Y, save_path='ridge_regression.gif'):
+    def animate(self, X, Y, save_path='lasso_regression.gif'):
         # Create a figure and axis for the animation
         fig, ax = plt.subplots(figsize=(10, 6))
         ax.scatter(X, Y, color='blue')
@@ -62,11 +61,7 @@ class RidgeRegression:
 
         # Save the animation as a GIF
         ani.save(save_path, writer=PillowWriter(fps=10))
-
+        plt.title('Lasso Regression')
         # Display the animation
         plt.show()
-
-
-    
-
 
